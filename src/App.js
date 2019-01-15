92e038de5b3c053e07b431ba8a1af949f1624b5f
unknown <githinvgeorge@gmail.com>
2019-01-15 10:50:30 +0530

    Testing my new branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi There. This is Githin.How is this possible.
+            Hi I have changed some code through Githins branch
           </p>
           <a
             className="App-link"
